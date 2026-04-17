29aa94e102f670dc567c8b92552231baa6bed081
Anvitha U S <anvithaus02@gmail.com>
2026-04-17 19:21:05 +0530

    feat: Added stove detection

--- Assets/Scripts/KitchenEnums.cs
+++ Assets/Scripts/KitchenEnums.cs
@@ -1,6 +1,14 @@
-public enum StationType 
-{ 
-    None, 
-    Table, 
-    Stove 
+public enum StationType
+{
+    None,
+    Table,
+    Stove
+}
+
+public enum CookingStationID
+{
+    Stove_Left,
+    Stove_Right,
+    Oven_Main,
+    DeepFryer
 }
